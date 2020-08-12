b5209758818ef1dcb66a0bfe8eca9872aa33d495
hopeless-vov <vip.vova556@gmail.com>
2020-08-12 23:55:52 +0300

    Updates

---
+++ precache-manifest.64f60181db48ac8c99038014143e4350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259e92e8ae6e2f244d361702a58bf1fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4945db98e76e768959",
+    "url": "/static/css/main.9d39fc57.chunk.css"
+  },
+  {
+    "revision": "51c436bdbd7f4051fee8",
+    "url": "/static/js/2.49e580cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49e580cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4945db98e76e768959",
+    "url": "/static/js/main.c4d7a6a0.chunk.js"
+  },
+  {
+    "revision": "79ab1c286997bf671012",
+    "url": "/static/js/runtime-main.009cc329.js"
+  }
+]);
